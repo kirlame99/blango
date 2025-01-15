1e777ee586b62705312865e2ae9dce8e1d13aac6
Nassourou Malik <malik.nassourou@yahoo.fr>
2025-01-15 19:30:31 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ["title","slug","published_at"]
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
