3000f5a2e3449d55fbfe60afd04e67fa3b95dfc3
Nassourou Malik <malik.nassourou@yahoo.fr>
2025-01-16 14:18:42 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
@@ -20,4 +21,30 @@ def author_details(author,current_user=None):
     return format_html('<a href="{}">{}</a>',author.email,name)
   else:
     return format_html('{}',name)
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">',extra_classes)
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+    
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">',extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.order_by('published_at').exclude(pk=post.pk)[:5]
+  return {
+    "posts" : posts,
+    "title" : "Recent Posts"
+  }
 
